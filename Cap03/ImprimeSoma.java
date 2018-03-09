eb0d2768a809fb1e588d087a281f67aae1e19ebb
Marcelo Carvalho <32714498+kvalhomanu@users.noreply.github.com>
2018-03-09 00:59:07 -0300

    Create ImprimeSoma.java

---
+++ Cap03/ImprimeSoma.java
@@ -0,0 +1,11 @@
+class ImprimeSoma{
+	public static void main(String [] args){
+		int soma = 0;
+		int numMin = 1;
+		int numMax = 1000;
+
+		soma = ((numMin + numMax) * numMax)/2;		
+		System.out.println(soma);
+
+	}
+}
