09f4399213b2a12d7dd92271a9d3b21d76a17e5b
Marcelo Carvalho <32714498+kvalhomanu@users.noreply.github.com>
2018-03-09 00:58:16 -0300

    Create Imprime.java

---
+++ Cap03/Imprime.java
@@ -0,0 +1,9 @@
+public class Imprime {
+    public static void main(String[] args) {    
+	int num1 = 150, num2 = 300;
+	while(num1 < num2){
+		num1 += 1;
+            System.out.println(num1);
+        }        
+    }
+} 
