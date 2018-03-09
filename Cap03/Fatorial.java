97d45565d3ebdd36084fed7743b77c99a9f2c915
Marcelo Carvalho <32714498+kvalhomanu@users.noreply.github.com>
2018-03-09 00:57:21 -0300

    Create Fatorial.java

---
+++ Cap03/Fatorial.java
@@ -0,0 +1,10 @@
+class Fatorial{
+	public static void main(String [] args){
+		int fatorial = 1;
+        	for (int n = 1; n <= 10; n++) {
+                	fatorial = fatorial * n;
+            	    System.out.println("Fatorial de "+n+" = "+fatorial);
+		}
+
+        }
+}
