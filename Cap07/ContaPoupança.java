a0db51668e52caa96d69dbec5fb52b5ff63f1dee
Marcelo Carvalho <32714498+kvalhomanu@users.noreply.github.com>
2018-03-09 00:55:55 -0300

    Create ContaPoupança.java

---
+++ Cap07/ContaPoupança.java
@@ -0,0 +1,11 @@
+package cap07;
+
+public class ContaPoupanca extends Conta{
+    
+    @Override
+    public void atualiza(double taxa){
+        this.saldo += this.saldo * taxa * 3;
+    }
+    
+    
+}
